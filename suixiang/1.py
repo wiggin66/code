77d6ecdbc2e7f1c5b1c86d557ef674d639f1ba02
wiggin66 <rcwanjinyu@163.com>
2023-06-12 23:03:43 +0800

    init suixiang

---
+++ suixiang/1.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+from typing import List
+
+class Solution:
+    def twoSum(self,nums:List[int],target:int)->List[int]:
+        n = len(nums)
+        for i in range (n):
+            for j in range(i+1,n):
+                if nums[i] + num[j]==target:
+                    return [i,j]
+        
